97d2215917388d63c032d9f3d85903cf7c3ca9a3
Christian Buchegger <christian.buchegger@softquadrat.de>
2019-09-12 08:55:49 +0200

    Add abort_session function
    (cherry picked from commit 301c591c9935461109b92cb73ef8743a4088631d)

--- h2/src/main/org/h2/expression/function/Function.java
+++ h2/src/main/org/h2/expression/function/Function.java
@@ -138,7 +138,8 @@ public class Function extends Expression implements FunctionCall {
             CANCEL_SESSION = 221, SET = 222, TABLE = 223, TABLE_DISTINCT = 224,
             FILE_READ = 225, TRANSACTION_ID = 226, TRUNCATE_VALUE = 227,
             NVL2 = 228, DECODE = 229, ARRAY_CONTAINS = 230, FILE_WRITE = 232,
-            UNNEST = 233, ARRAY_CONCAT = 234, ARRAY_APPEND = 235, ARRAY_SLICE = 236;
+            UNNEST = 233, ARRAY_CONCAT = 234, ARRAY_APPEND = 235, ARRAY_SLICE = 236,
+            ABORT_SESSION = 237;
 
     public static final int REGEXP_LIKE = 240;
 
@@ -453,6 +454,8 @@ public class Function extends Expression implements FunctionCall {
         addFunctionWithNull("GREATEST", GREATEST,
                 VAR_ARGS, Value.NULL);
         addFunctionNotDeterministic("CANCEL_SESSION", CANCEL_SESSION,
+                1, Value.BOOLEAN);
+        addFunctionNotDeterministic("ABORT_SESSION", ABORT_SESSION,
                 1, Value.BOOLEAN);
         addFunction("SET", SET,
                 2, Value.NULL, false, false, true, true);
@@ -1111,6 +1114,10 @@ public class Function extends Expression implements FunctionCall {
             result = ValueBoolean.get(cancelStatement(session, v0.getInt()));
             break;
         }
+        case ABORT_SESSION: {
+            result = ValueBoolean.get(abortSession(session, v0.getInt()));
+            break;
+        }
         case TRANSACTION_ID: {
             result = session.getTransactionId();
             break;
@@ -1142,6 +1149,22 @@ public class Function extends Expression implements FunctionCall {
                     return false;
                 }
                 c.cancel();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean abortSession(Session session, int targetSessionId) {
+        session.getUser().checkAdmin();
+        Session[] sessions = session.getDatabase().getSessions(false);
+        for (Session s : sessions) {
+            if (s.getId() == targetSessionId) {
+                Command c = s.getCurrentCommand();
+                if (c != null) {
+                    c.cancel();
+                }
+                s.close();
                 return true;
             }
         }
