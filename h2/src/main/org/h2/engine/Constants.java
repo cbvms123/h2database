58f021fe34734c49c5aca252d49a595d7904a449
Christian Buchegger <cbvms123@googlemail.com>
2019-11-27 11:53:57 +0100

    Add vendor and patch info

--- h2/src/main/org/h2/engine/Constants.java
+++ h2/src/main/org/h2/engine/Constants.java
@@ -43,7 +43,7 @@ public class Constants {
      * Additionally, a version number should be set to distinguish releases.
      * Example: ACME_SVN1651_BUILD3
      */
-    public static final String BUILD_VENDOR_AND_VERSION = null;
+    public static final String BUILD_VENDOR_AND_VERSION = "Softquadrat-P1";
 
     /**
      * The TCP protocol version number 9.
